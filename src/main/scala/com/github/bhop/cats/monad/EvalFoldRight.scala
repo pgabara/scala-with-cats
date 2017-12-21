7290e6e10d3232f9f0d9fd432712da883960e113
Piotr Gabara <piotr.gabara@hotmail.com>
2017-12-21 21:56:28 +0100

    Exercise: Safer folding using Eval

---
+++ src/main/scala/com/github/bhop/cats/monad/EvalFoldRight.scala
@@ -0,0 +1,14 @@
+package com.github.bhop.cats.monad
+
+object EvalFoldRight {
+
+  import cats.Eval
+
+  def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): Eval[B] =
+    as match {
+      case head :: tail =>
+        Eval.defer(foldRight(tail, acc)(fn)).map(fn(head, _))
+      case _ =>
+        Eval.now(acc)
+    }
+}
---
+++ src/test/scala/com/github/bhop/cats/monad/EvalFoldRightSpec.scala
@@ -0,0 +1,23 @@
+package com.github.bhop.cats.monad
+
+import org.scalatest.{Matchers, WordSpec}
+
+class EvalFoldRightSpec extends WordSpec with Matchers {
+
+  import EvalFoldRight._
+
+  "An Eval fold right" should {
+
+    "fold non empty list" in {
+      foldRight[Int, Int](List(1, 2, 3, 4, 5), 0)(_ + _).value should be(15)
+    }
+
+    "fold list with only one element" in {
+      foldRight[Int, Int](List(5), 0)(_ + _).value should be(5)
+    }
+
+    "fold empty list" in {
+      foldRight[Int, Int](List.empty, 0)(_ + _).value should be(0)
+    }
+  }
+}
